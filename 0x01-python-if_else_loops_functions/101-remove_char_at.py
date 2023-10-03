23d0e85b38dd1268a59cb32bc843b7279f4f28a4
ib-babs <babatundeibrahim426@gmail.com>
2023-10-04 00:37:22 +0100

    Initial commit

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    idx = 0
-    arr = [0] * 1024
-    for l in str:
-        arr[idx] = l
-        idx += 1
-    arr = arr[:idx]
-    return arr
-print(remove_char_at("Best School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
+    if n >= 0:
+        return str[:n] + str[n + 1:]
+    return str
