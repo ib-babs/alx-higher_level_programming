d0fcc01feadee07723609aad1c13fdeeb48bd6fd
ib-babs <babatundeibrahim426@gmail.com>
2023-10-04 00:16:14 +0100

    Initial commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for j in range(1, 10):
-        if j < i or j == i:
-            continue
-        if j > 1:
-            print(", ", end='')
-        print(f"{i}{j}", end='')
-print()
+
+for a in range(0, 10):
+    for b in range(a + 1, 10):
+        if a == 8 and b == 9:
+            print("89")
+        elif a != b:
+            print("{:d}{:d}".format(a, b), end=', ')
