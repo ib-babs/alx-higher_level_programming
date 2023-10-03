dd93403aee799bf053edd778fdd71ec6ec97233f
ib-babs <babatundeibrahim426@gmail.com>
2023-10-03 23:49:32 +0100

    Initial commit

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+# 2-print_alphabet.py
+# Babatunde Ibrahim
+
+"""Print the alphabet in lowercase, not followed by a new line."""
 for letter in range(97, 123):
     print(f"{chr(letter)}", end="")
