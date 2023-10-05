684b0cfcde0ae229251eae12d6b97422c07b72c8
ib-babs <babatundeibrahim426@gmail.com>
2023-10-05 18:03:48 +0100

    Second commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
+
+
 def arg_calc(argv):
     argv_len = len(argv)
     a = int(argv[1])
@@ -9,16 +11,17 @@ def arg_calc(argv):
         print(f"Usage: {argv[0]} <a> <operator> <b>")
         exit(1)
     if operator == "+":
-          print("{:d} {:s} {:d} = {:d}".format(a, operator, b, add(a, b)))
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, add(a, b)))
     elif operator == "-":
         print("{:d} {:s} {:d} = {:d}".format(a, operator, b, sub(a, b)))
     elif operator == "*":
         print("{:d} {:s} {:d} = {:d}".format(a, operator, b, mul(a, b)))
     elif operator == "/":
-            print("{:d} {:s} {:d} = {:d}".format(a, operator, b, div(a, b)))
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+
 
 if __name__ == "__main__":
     import sys
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,9 @@ def infinite_num():
     from sys import argv
     sum = 0
     for arg in argv[1:]:
-        sum = sum + int(arg);
+        sum = sum + int(arg)
     print(sum)
+
 
 if __name__ == "__main__":
     infinite_num()
