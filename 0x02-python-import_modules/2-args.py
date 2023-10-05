0ee9fa75f2aa5899267833e0491a95d1653b6e95
ib-babs <babatundeibrahim426@gmail.com>
2023-10-05 18:12:46 +0100

    New

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
-def print_args():
-    from sys import argv
+def print_args(argv):
     argv_len = len(argv)
     i = 1
     if argv_len == 1:
         print("0 argument.")
     elif argv_len == 2:
-        print("1 argument:\n1: {}".format(argv[1]))
+        print("1 argument:\n1: {:s}".format(argv[1]))
     else:
         print("{} arguments:".format(argv_len - 1))
         for arg in argv[1:]:
@@ -16,4 +15,5 @@ def print_args():
 
 
 if __name__ == "__main__":
-    print_args()
+    from sys import argv
+    print_args(argv)
