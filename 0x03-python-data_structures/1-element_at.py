f88a9b6f1220851ce0983142b5f4d73324c82782
ib-babs <babatundeibrahim426@gmail.com>
2023-10-07 20:06:20 +0100

    Initial commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,4 +3,4 @@
 
 # Print element at index idx
 def element_at(my_list, idx):
-    return(my_list[idx] if 0 <= idx < len(my_list) else "None")
+    return(my_list[idx] if 0 < idx < len(my_list) else "None")
