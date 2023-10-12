67fbd2c82bf5d25a52cd0d3e2f8f08e35ebda638
ib-babs <babatundeibrahim426@gmail.com>
2023-10-12 11:58:18 +0100

    New Commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return (None)
-    best_score_search = list(a_dictionary.keys())[0]
-    for key, value in a_dictionary.items():
-        if best_score_search < key:
-            best_score_search = key
-    return best_score_search
+    else:
+        v = list(a_dictionary.values())
+        k = list(a_dictionary.keys())
+        return (k[v.index(max(v))])
