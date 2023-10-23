8329fc887dca00eff6b1de47dc236c68e024e3cd
ib-babs <babatundeibrahim426@gmail.com>
2023-10-23 20:33:35 +0100

    Newer Commit

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     try:
         fct(*args)
+        return fct(*args)
     except Exception as E:
         sys.stderr.write("Exception: {}\n".format(E))
         return None
-    return fct(*args)
