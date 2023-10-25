8ab173e7b1b47c48d96b6214b5ff675e63457946
ib-babs <babatundeibrahim426@gmail.com>
2023-10-25 21:07:14 +0100

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,12 +48,8 @@ class Square:
             print()
         else:
             for i in range(0, size):
-                # if pos[1] > 1:
-                #     pass
-                # else:
                 print(pos[0] * " ", end='')
                 print(size * "#")
-        print()
 
     @property
     def position(self):
