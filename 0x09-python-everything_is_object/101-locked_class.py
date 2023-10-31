2e67b5be0577a136c91d955a8ca1c7cb9d2af05a
ib-babs <babatundeibrahim426@gmail.com>
2023-10-31 17:57:34 +0100

    Initial commit

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""LockedClass class"""
+
+
+class LockedClass:
+    """Variable declaration"""
+    __slots__ = ("first_name")
---
+++ 0x09-python-everything_is_object/README.md
@@ -0,0 +1,4 @@
+# .txt Answer Files
+1. Only one line
+2. No Shebang
+3. All your files should end with a new line
