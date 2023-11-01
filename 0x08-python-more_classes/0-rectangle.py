dc127289db448ca2d0371db5ee1aad29c7a4201f
ib-babs <babatundeibrahim426@gmail.com>
2023-11-01 17:29:17 +0100

    Inital commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Rectangle is an class that defines a rectangle
+Operations:
+    Height and width of the rectangle
+    Find the area of rectangle"""
+
+
+class Rectangle:
+    """Define Rectangle class"""
---
+++ 0x08-python-more_classes/9-rectangle.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+"""Rectangle is an class that defines a rectangle
+Operations:
+    Height and width of the rectangle
+    Find the area of rectangle"""
+
+
+class Rectangle:
+    """Define Rectangle class"""
+    number_of_instances = 0  # class attribute - number_of_instances
+    print_symbol = "#"  # class attribute - print_symbol
+
+    def __init__(self, width=0, height=0) -> None:
+        """Initalize variable"""
+        self.__width = width
+        self.__height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self) -> int:
+        """Getting width property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setting width property"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self) -> int:
+        """Getting height property"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setting height property"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        "Find the area of rectangle"
+        return self.__width * self.__height
+
+    def perimeter(self) -> int:
+        """Find the perimeter of rectangle"""
+        pmt: int = None
+        if self.__width == 0 or self.__height == 0:
+            pmt = 0
+        else:
+            pmt = (self.__width * 2) + (self.__height * 2)
+        return pmt
+
+    def __str__(self):
+        """String representation of the class: Rectangle"""
+        hash_chr = ''
+        if self.__width == 0 or self.__height == 0:
+            return ''
+        for _ in range(self.__height):
+            for _ in range(self.__width):
+                hash_chr += f"{self.print_symbol}"
+            hash_chr += '\n'
+        return hash_chr[:-1]
+
+    def __repr__(self) -> str:
+        """Return string representation of object
+        This can recreate a new instance with `eval` func"""
+        return f"{type(self).__name__}({self.__width}, {self.__height})"
+
+    def __del__(self):
+        """Operation to perform when the class instance is deleted"""
+        Rectangle.number_of_instances -= 1
+        print("Bye Rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Compare two Rectangle class"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        big_rect = None
+        if rect_1.area() > rect_2.area():
+            big_rect = rect_1
+        elif rect_1.area() < rect_2.area():
+            big_rect = rect_2
+        else:
+            big_rect = rect_1
+        return big_rect
+
+    @classmethod
+    def square(cls, size=0):
+        """Returns a new Rectangle instance"""
+        cls_width = cls_height = size
+        return cls(cls_width, cls_height)
