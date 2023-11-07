d9a6becc653695ed1b20160153990abff3352b49
ib-babs <babatundeibrahim426@gmail.com>
2023-11-07 19:28:30 +0100

    Initial commit

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,5 +6,6 @@ import json
 
 
 def load_from_json_file(filename):
+    """load_from_json_file function"""
     with open(filename) as f:
         return (json.load(f))
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""
+Function module
+"""
 import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 def add_item():
+    """add_item function"""
     arg_list = [i for i in sys.argv[1:]]
 
     save_to_json_file(arg_list, "add_item.json")
