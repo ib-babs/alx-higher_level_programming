db8fc0cb3c17862579042a7730ff3c125dc337be
ib-babs <babatundeibrahim426@gmail.com>
2023-11-13 21:46:14 +0100

    Newest commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,7 +21,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """Return JSON string representation"""
         if list_dictionaries is None or list_dictionaries == []:
-            return "[]"
+            return []
         return json.dumps(list_dictionaries)
 
     @classmethod
