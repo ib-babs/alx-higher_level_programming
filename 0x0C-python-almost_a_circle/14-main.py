5e72468eb40afe5b8589f10fadd7773f15958ac7
ib-babs <babatundeibrahim426@gmail.com>
2023-11-14 22:02:19 +0100

    Newer commit

--- 0x0C-python-almost_a_circle/14-main.py
+++ 0x0C-python-almost_a_circle/14-main.py
@@ -1,45 +1,60 @@
 #!/usr/bin/python3
 
 """ 14-main """
+import random
+import json
+import os
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 import turtle
+#!/usr/bin/python3
+""" Check """
 
-if __name__ == "__main__":
 
-    r1 = Rectangle(10, 7, 2, 8)
-    r2 = Rectangle(2, 4)
-    list_rectangles_input = [r1, r2]
+file_path = "Square.json"
+if os.path.exists(file_path):
+    os.remove(file_path)
 
-    Rectangle.save_to_file_csv(list_rectangles_input)
+list_objs = []
+for i in range(0, 5):
+    list_objs.append(Square(random.randrange(1, 100, 2),
+                     random.randrange(1, 100, 2), random.randrange(1, 100, 2)))
 
-    list_rectangles_output = Rectangle.load_from_file_csv()
+Square.save_to_file(list_objs)
 
-    for rect in list_rectangles_input:
-        print("[{}] {}".format(id(rect), rect))
+list_objs_res = Square.load_from_file()
 
-    print("---")
+if list_objs_res is None:
+    print("load_from_file doesn't return a list")
+    exit(1)
 
-    for rect in list_rectangles_output:
-        print("[{}] {}".format(id(rect), rect))
+if len(list_objs_res) != len(list_objs):
+    print("load_from_file doesn't return a list")
+    exit(1)
 
-    print("---")
-    print("---")
+for i in range(0, len(list_objs)):
+    obj = list_objs[i]
+    if i >= len(list_objs_res):
+        print("load_from_file doesn't return all objects")
+        exit(1)
 
-    s1 = Square(5)
-    s2 = Square(7, 9, 1)
-    list_squares_input = [s1, s2]
+    obj_res = list_objs_res[i]
 
-    Square.save_to_file_csv(list_squares_input)
+    if obj_res.id != obj.id:
+        print("Square {} not found".format(obj))
+        exit(1)
 
-    list_squares_output = Square.load_from_file_csv()
+    if obj_res.size != obj.size:
+        print("Square {} doesn't match with the original: {}".format(obj_res, obj))
+        exit(1)
 
-    for square in list_squares_input:
-        print("[{}] {}".format(id(square), square))
+    if obj_res.x != obj.x:
+        print("Square {} doesn't match with the original: {}".format(obj_res, obj))
+        exit(1)
 
-    print("---")
+    if obj_res.y != obj.y:
+        print("Square {} doesn't match with the original: {}".format(obj_res, obj))
+        exit(1)
 
-    for square in list_squares_output:
-        print("[{}] {}".format(id(square), square))
-
+print("OK", end="")
