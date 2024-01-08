e1c67964d7c1c2a5f79563ff2ffd3e21eea2cd0a
ib-babs <babatundeibrahim426@gmail.com>
2024-01-08 13:46:30 +0100

    Initial commit

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 const processArgv = process.argv;
-if (processArgv.length < 3) console.log('No argument');
+let recordLength;
+for (recordLength = 0; processArgv[recordLength]; recordLength++);
+
+if (recordLength < 3) console.log('No argument');
 else {
   let i;
-  for (i = 2; i < processArgv.length; i++) console.log(processArgv[i]);
+  for (i = 2; i < recordLength; i++) console.log(processArgv[i]);
 }
