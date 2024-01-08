e8d567e93c90e74ac601fd1e54fafb0c090335fe
ib-babs <babatundeibrahim426@gmail.com>
2024-01-08 13:35:14 +0100

    Initial commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const processArgv = require('node:process').argv;
-if (processArgv.length < 3) console.log('No argument');
-else console.log('Argument found');
+const processArgv = process.argv;
+if (processArgv.length < 3) console.log("No argument");
+else console.log("Argument found");
