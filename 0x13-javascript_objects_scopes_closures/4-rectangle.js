afea2dc99107795e9b58693d95f4df94ec6c11bf
ib-babs <babatundeibrahim426@gmail.com>
2024-01-09 18:11:11 +0100

    Initial commit

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,22 +1,23 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
-  print() {
+
+  print () {
     for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 
-  rotate() {
+  rotate () {
     const holder = this.width;
     this.width = this.height;
     this.height = holder;
   }
 
-  double() {
+  double () {
     this.width = this.width * 2;
     this.height = this.height * 2;
   }
