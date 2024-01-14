12638411a004ba3cc34341e0d309661c8f60a16d
ib-babs <babatundeibrahim426@gmail.com>
2024-01-14 16:46:03 +0100

    Initial commit

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,32 +3,22 @@
 Fetch all from state"""
 
 
-def fetch_all_from_state():
-    try:
-        url = {'drivername': 'mysql+mysqldb',
-               'username': argv[1],
-               'password': argv[2],
-               'host': '127.0.0.1',
-               'port': 3306,
-               'database': argv[3]
-               }
-        engine = create_engine(URL.create(**url))
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        res = session.query(State.id, State.name).order_by(State.id).all()
-        session.close()
-
-        for id, name in res:
-            print('{}: {}'.format(id, name))
-    except Exception as e:
-        pass
+def fetch_all_from_state(engine):
+    """Fetch all states columns"""
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    res = session.query(State.id, State.name).order_by(State.id).all()
+    for id, name in res:
+        print('{}: {}'.format(id, name))
+    session.close()
 
 
 if __name__ == "__main__":
     from model_state import (State, Base)
-    from sqlalchemy.orm import (sessionmaker,)
+    from sqlalchemy.orm import (sessionmaker)
     from sqlalchemy import create_engine
-    from sqlalchemy.engine.url import URL
     from sys import argv
-    fetch_all_from_state()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    fetch_all_from_state(engine=engine)
