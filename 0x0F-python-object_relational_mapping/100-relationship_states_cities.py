c1281ac52d789a66b40212d03d7f1318694ac410
ib-babs <babatundeibrahim426@gmail.com>
2024-01-14 17:12:46 +0100

    Initial commit

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -5,31 +5,24 @@ creates the State “California” with the City “San Francisco”
 from the database hbtn_0e_100_usa"""
 
 
-def relationship_state_city():
-    try:
-        url = {'drivername': 'mysql+mysqldb',
-               'username': argv[1],
-               'password': argv[2],
-               'host': '127.0.0.1',
-               'port': 3306,
-               'database': argv[3]
-               }
-        engine = create_engine(URL.create(**url))
-        state = State(name="California", cities=[City(name="San Francisco")])
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        session.add(state)
-        session.commit()
-        session.close()
-    except Exception as e:
-        print(e)
+def relationship_state_city(engine):
+    """Create related data"""
+    Base.metadata.create_all(engine)
+
+    state = State(name="California", cities=[City(name="San Francisco")])
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(state)
+    session.commit()
+    session.close()
 
 
 if __name__ == "__main__":
+    from relationship_state import (State, Base)
     from relationship_city import City
-    from relationship_state import State
-    from sqlalchemy.orm import (sessionmaker,)
+    from sqlalchemy.orm import (sessionmaker)
     from sqlalchemy import create_engine
-    from sqlalchemy.engine.url import URL
     from sys import argv
-    relationship_state_city()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    relationship_state_city(engine=engine)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -5,28 +5,19 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import relationship
 from sys import argv
-from sqlalchemy.engine.url import URL
 
 Base = declarative_base()
-try:
-    url = {'drivername': 'mysql+mysqldb',
-           'username': argv[1],
-           'password': argv[2],
-           'host': '127.0.0.1',
-           'port': 3306,
-           'database': argv[3]
-           }
-    engine = create_engine(URL.create(**url))
 
-    class State(Base):
-        """Derived class of Base"""
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True, nullable=False)
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade="all, delete, delete-orphan")
 
-    Base.metadata.create_all(engine)
+class State(Base):
+    """Derived class of Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state',
+                          cascade="all, delete, delete-orphan")
 
-except Exception as e:
-    pass
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
