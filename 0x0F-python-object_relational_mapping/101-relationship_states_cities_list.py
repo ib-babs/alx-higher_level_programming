fde57bf52615669802bb85845ea123ef8dd65af5
ib-babs <babatundeibrahim426@gmail.com>
2024-01-14 17:16:43 +0100

    Initial commit

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -5,24 +5,19 @@ lists all State objects, and corresponding City objects,
 contained in the database hbtn_0e_101_usa"""
 
 
-def relationship_state_city():
-    try:
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-            argv[1], argv[2], argv[3]), pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-        states = State
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        rows = session.query(states).join(
-            City).order_by(states.id, City.id).all()
-        for row in rows:
-            print("{}: {}".format(row.id, row.name))
-            for city in row.cities:
-                print("    {}: {}".format(city.id, city.name))
+def relationship_state_city(engine):
+    Base.metadata.create_all(engine)
+    states = State
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    rows = session.query(states).join(
+        City).order_by(states.id, City.id).all()
+    for row in rows:
+        print("{}: {}".format(row.id, row.name))
+        for city in row.cities:
+            print("    {}: {}".format(city.id, city.name))
 
-        session.close()
-    except Exception as e:
-        print(e)
+    session.close()
 
 
 if __name__ == "__main__":
@@ -30,6 +25,7 @@ if __name__ == "__main__":
     from relationship_state import State, Base
     from sqlalchemy.orm import (sessionmaker)
     from sqlalchemy import create_engine
-    from sqlalchemy.engine.url import URL
     from sys import argv
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     relationship_state_city()
