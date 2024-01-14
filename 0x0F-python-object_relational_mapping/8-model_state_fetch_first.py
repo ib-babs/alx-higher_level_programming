a66385b3b5d835854deb6bc1ea0d40693529cd0b
ib-babs <babatundeibrahim426@gmail.com>
2024-01-14 16:49:06 +0100

    Initial commit

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,33 +3,24 @@
 Fetch from state"""
 
 
-def fetch_first_from_state():
-    try:
-        url = {'drivername': 'mysql+mysqldb',
-               'username': argv[1],
-               'password': argv[2],
-               'host': '127.0.0.1',
-               'port': 3306,
-               'database': argv[3]
-               }
-        engine = create_engine(URL.create(**url))
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        res = session.query(State.id, State.name).first()
-        if len(res) > 1:
-            print('{}: {}'.format(res[0], res[1]))
-        else:
-            print("Nothing")
-        session.close()
-    except Exception as e:
-        pass
+def fetch_first_from_state(engine):
+    """Fetch first row"""
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    res = session.query(State.id, State.name).first()
+    if len(res) > 1:
+        print('{}: {}'.format(res[0], res[1]))
+    else:
+        print("Nothing")
+    session.close()
 
 
 if __name__ == "__main__":
     from model_state import (State, Base)
-    from sqlalchemy.orm import (sessionmaker,)
+    from sqlalchemy.orm import (sessionmaker)
     from sqlalchemy import create_engine
-    from sqlalchemy.engine.url import URL
     from sys import argv
-    fetch_first_from_state()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    fetch_first_from_state(engine=engine)
