7504c6b3401012abef20232df1987c196ad4fd88
ib-babs <babatundeibrahim426@gmail.com>
2024-01-14 16:38:35 +0100

    Initial commit

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """ORM - Object Relational Mapping Application
 Modelling State class"""
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, create_engine
 from sys import argv
 from sqlalchemy.engine.url import URL
 
 Base = declarative_base()
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-    argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
 
 class State(Base):
@@ -16,3 +15,7 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    argv[1], argv[2], argv[3]), pool_pre_ping=True)
