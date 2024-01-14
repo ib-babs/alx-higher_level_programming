02c6f3e87c255d6054a25d520f9cb6b30fd92935
ib-babs <babatundeibrahim426@gmail.com>
2024-01-14 17:09:11 +0100

    Initial commit

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,27 +4,17 @@ Modelling City class"""
 from model_state import Base
 from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
 from sys import argv
-from sqlalchemy.engine.url import URL
 
-try:
-    url = {'drivername': 'mysql+mysqldb',
-           'username': argv[1],
-           'password': argv[2],
-           'host': '127.0.0.1',
-           'port': 3306,
-           'database': argv[3]
-           }
-    engine = create_engine(URL.create(**url))
 
-    class City(Base):
-        """Derived class of Base class"""
-        __tablename__ = 'cities'
-        id = Column(Integer, primary_key=True, nullable=False)
-        name = Column(String(128), nullable=False)
-        state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+class City(Base):
+    """Derived class of Base class"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
 
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-
-except Exception as e:
-    pass
