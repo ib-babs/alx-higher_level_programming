2177213488aaad0fff274800d7d8fe6e962b2cd7
ib-babs <babatundeibrahim426@gmail.com>
2024-01-14 16:56:13 +0100

    Initial commit

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,36 +1,28 @@
 #!/usr/bin/python3
 """ORM - Object Relational Mapping Application
-Fetch rows that contain 'a' from state"""
+prints the State object with the name passed as
+argument from the database hbtn_0e_6_usa"""
 
 
-def get_id():
-    try:
-        url = {'drivername': 'mysql+mysqldb',
-               'username': argv[1],
-               'password': argv[2],
-               'host': '127.0.0.1',
-               'port': 3306,
-               'database': argv[3]
-               }
-        engine = create_engine(URL.create(**url))
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        res = session.query(State.id).filter(
-            State.name == argv[4]).scalar()
-        if (not res):
-            print("Not found")
-        else:
-            print(res)
-        session.close()
-    except Exception as e:
-        pass
+def get_id(engine):
+    """Get row by id"""
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    res = session.query(State.id).filter(
+        State.name == argv[4]).scalar()
+    if (not res):
+        print("Not found")
+    else:
+        print(res)
+    session.close()
 
 
 if __name__ == "__main__":
     from model_state import (State, Base)
-    from sqlalchemy.orm import (sessionmaker,)
+    from sqlalchemy.orm import (sessionmaker)
     from sqlalchemy import create_engine
-    from sqlalchemy.engine.url import URL
     from sys import argv
-    get_id()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    get_id(engine=engine)
