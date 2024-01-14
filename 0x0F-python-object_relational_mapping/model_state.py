dfdf4ceca13154f937c4e3b074bf98f56a38fc56
ib-babs <babatundeibrahim426@gmail.com>
2024-01-14 16:32:06 +0100

    Initial commit

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,18 +7,18 @@ from sys import argv
 from sqlalchemy.engine.url import URL
 
 Base = declarative_base()
-if (len(argv) == 4):
-    url = {'drivername': 'mysql+mysqldb',
-           'username': argv[1],
-           'password': argv[2],
-           'host': '127.0.0.1',
-           'port': 3306,
-           'database': argv[3]
-           }
-    engine = create_engine(URL.create(**url))
+url = {'drivername': 'mysql+mysqldb',
+       'username': argv[1],
+       'password': argv[2],
+       'host': '127.0.0.1',
+       'port': 3306,
+       'database': argv[3]
+       }
+engine = create_engine(URL.create(**url))
 
-    class State(Base):
-        """Derived class of Base"""
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True, nullable=False)
-        name = Column(String(128), nullable=False)
+
+class State(Base):
+    """Derived class of Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
