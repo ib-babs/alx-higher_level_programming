d72ad14dfa0a0b46330e8cad477a31b64375aab4
ib-babs <babatundeibrahim426@gmail.com>
2024-01-14 17:14:06 +0100

    Initial commit

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ORM - Object Relational Mapping Application
 Modelling City class"""
-from model_state import Base
+from relationship_state import Base
 from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
 from sys import argv
 
@@ -17,4 +17,3 @@ class City(Base):
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
