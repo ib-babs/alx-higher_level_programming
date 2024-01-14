fe3fed7b9e96b554969554cf62a55ba197bd3e6e
ib-babs <babatundeibrahim426@gmail.com>
2024-01-14 17:17:36 +0100

    Initial commit

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -28,4 +28,4 @@ if __name__ == "__main__":
     from sys import argv
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    relationship_state_city()
+    relationship_state_city(engine=engine)
