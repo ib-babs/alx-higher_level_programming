c9df1f8e5596bda53ff8001f27f66a9c970fe28c
ib-babs <babatundeibrahim426@gmail.com>
2024-01-14 17:00:51 +0100

    Initial commit

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -3,31 +3,22 @@
 Delete data from `states` table"""
 
 
-def delete_column_data():
-    try:
-        url = {'drivername': 'mysql+mysqldb',
-               'username': argv[1],
-               'password': argv[2],
-               'host': '127.0.0.1',
-               'port': 3306,
-               'database': argv[3]
-               }
-        engine = create_engine(URL.create(**url))
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        res = session.query(State).filter(State.name.contains('a')).all()
-        for row in res:
-            session.delete(row)
-        session.commit()
-        session.close()
-    except Exception as e:
-        pass
+def delete_column_data(engine):
+    """Delete column"""
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    res = session.query(State).filter(State.name.contains('a')).all()
+    for row in res:
+        session.delete(row)
+    session.commit()
+    session.close()
 
 
 if __name__ == "__main__":
     from model_state import (State, Base)
-    from sqlalchemy.orm import (sessionmaker,)
+    from sqlalchemy.orm import (sessionmaker)
     from sqlalchemy import create_engine
-    from sqlalchemy.engine.url import URL
     from sys import argv
-    delete_column_data()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    delete_column_data(engine=engine)
