8498424369a65c594688b3ad045517ab263f65d1
ib-babs <babatundeibrahim426@gmail.com>
2024-01-14 17:02:37 +0100

    Initial commit

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,33 +4,24 @@ prints all City objects
 from the database hbtn_0e_14_usa"""
 
 
-def fetch_all_from_city():
-    try:
-        url = {'drivername': 'mysql+mysqldb',
-               'username': argv[1],
-               'password': argv[2],
-               'host': '127.0.0.1',
-               'port': 3306,
-               'database': argv[3]
-               }
-        engine = create_engine(URL.create(**url))
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        res = session.query(State.name, City.id, City.name).join(
-            City, State.id == City.state_id).order_by(City.id)
-        for state_name, city_id, city_name in res.all():
-            print('{}: ({}) {}'.format(state_name, city_id, city_name))
-        session.close()
-    except Exception as e:
-        pass
+def fetch_all_from_city(engine):
+    """Print all city object"""
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    res = session.query(State.name, City.id, City.name).join(
+        City, State.id == City.state_id).order_by(City.id)
+    for state_name, city_id, city_name in res.all():
+        print('{}: ({}) {}'.format(state_name, city_id, city_name))
+    session.close()
 
 
 if __name__ == "__main__":
-    from model_city import City
     from model_state import (State, Base)
-    from sqlalchemy.orm import (sessionmaker,)
+    from model_city import City
+    from sqlalchemy.orm import (sessionmaker)
     from sqlalchemy import create_engine
-    from sqlalchemy.engine.url import URL
     from sys import argv
-    fetch_all_from_city()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    fetch_all_from_city(engine=engine)
