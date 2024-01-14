044ec20b79a21fdcb16955ec61d1f9ace0627359
ib-babs <babatundeibrahim426@gmail.com>
2024-01-14 16:53:17 +0100

    Initial commit

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -3,32 +3,23 @@
 Fetch rows that contain 'a' from state"""
 
 
-def fetch_a_from_state():
-    try:
-        url = {'drivername': 'mysql+mysqldb',
-               'username': argv[1],
-               'password': argv[2],
-               'host': '127.0.0.1',
-               'port': 3306,
-               'database': argv[3]
-               }
-        engine = create_engine(URL.create(**url))
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        res = session.query(State.id, State.name).filter(
-            State.name.contains("a"))
-        for id, name in res.all():
-            print('{}: {}'.format(id, name))
-        session.close()
-    except Exception as e:
-        pass
+def fetch_a_from_state(engine):
+    """Fetch rows contain letter 'a'"""
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    res = session.query(State.id, State.name).filter(
+        State.name.contains("a"))
+    for id, name in res.all():
+        print('{}: {}'.format(id, name))
+    session.close()
 
 
 if __name__ == "__main__":
     from model_state import (State, Base)
-    from sqlalchemy.orm import (sessionmaker,)
+    from sqlalchemy.orm import (sessionmaker)
     from sqlalchemy import create_engine
-    from sqlalchemy.engine.url import URL
     from sys import argv
-    fetch_a_from_state()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    fetch_a_from_state(engine=engine)
