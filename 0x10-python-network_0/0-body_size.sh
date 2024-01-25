4a7fa618f380132ba80828487213c1fb5282691b
ib-babs <babatundeibrahim426@gmail.com>
2024-01-25 18:32:47 +0100

    Initial commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Takes in a URL, sends a request
-# to that URL, and displays the size of the body of the response
+# Takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-#curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}'
 size=$(curl -s -w '%{size_download}\n' -o /dev/null "$1")
 echo "$size"
