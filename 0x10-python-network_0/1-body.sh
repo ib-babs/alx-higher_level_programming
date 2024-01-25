6096ddffa3bfe1929f24ee4173ada45b0a1391a7
ib-babs <babatundeibrahim426@gmail.com>
2024-01-25 20:15:46 +0100

    Initial commit

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,8 +2,4 @@
 # Takes in a URL, sends a request to that URL, and displays the body of the response if
 # status code is 200
 # statusCode=$
-curl -sI "$1" | grep 'HTTP/[0-9].[0-9]' | cut -d' ' -f2
-
-# if [ "$statusCode" -eq 200 ]; then
-#     curl -s "$1"
-# fi
+curl -sL "$1"
