a9969d4ff87a80cb527169d7aa824f6fbd2c6853
ib-babs <babatundeibrahim426@gmail.com>
2024-01-25 18:53:33 +0100

    Initial commit

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the status code of body of the response
-curl -sI "$1" | grep 'HTTP/[0-1].[0-9]' | cut -d' ' -f2
+curl -s "$1"
