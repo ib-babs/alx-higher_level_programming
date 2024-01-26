3e3a2183d1167e8d5c81e3aca94c3e40c7a59902
ib-babs <babatundeibrahim426@gmail.com>
2024-01-26 19:41:42 +0100

    New commit

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+"""Python script that takes in a URL,
+sends a request to the URL
+and displays the body of the response"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    try:
+        req = requests.get(url=url)
+        print(req.text)
+    except requests.exceptions.HTTPError as e:
+        print("Error code: {}".format(e.code))
