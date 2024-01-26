ef1809de425817e055c865314bc95534c1c0cd21
ib-babs <babatundeibrahim426@gmail.com>
2024-01-26 19:37:43 +0100

    New commit

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    req = requests.post(
+        url=url,
+        data={"email": email},
+    )
+
+    print(req.text)
