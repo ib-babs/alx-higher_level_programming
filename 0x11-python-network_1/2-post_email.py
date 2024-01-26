b7f3fdac25d537ce938e03e3e93790e82e07864f
ib-babs <babatundeibrahim426@gmail.com>
2024-01-26 19:21:53 +0100

    Initial commit

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     req = urllib.request.Request(
-        url=url, method='POST',
+        url=url,
         data={"email": email},
+        method='POST',
     )
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status
+using `requests` package"""
+if __name__ == "__main__":
+    import requests
+    url = "https://alx-intranet.hbtn.io/status"
+    req = requests.get(url)
+    content = req.content.decode('utf-8')
+    res = """Body response:
+    - type: {}
+    - content: {}""".format(type(content), content)
+    print(res)
