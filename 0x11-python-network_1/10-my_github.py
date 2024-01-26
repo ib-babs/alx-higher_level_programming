8589980e0028f0b4ed5e4adaf9fd01713b752393
ib-babs <babatundeibrahim426@gmail.com>
2024-01-26 20:56:02 +0100

    New commit

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    try:
+        headers = {"Accept": "application/vnd.github+json",
+                   "Authorization": "Bearer {}".format(sys.argv[2])
+                   }
+        url = "https://api.github.com/users/{}".format(sys.argv[1])
+        req = requests.get(url, headers=headers)
+
+        print(req.json().get('id'))
+    except ValueError:
+        pass
