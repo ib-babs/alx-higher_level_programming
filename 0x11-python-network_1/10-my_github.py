0aceb79517469e9b4b709552c4c7cb4af8e5c3c3
ib-babs <babatundeibrahim426@gmail.com>
2024-01-26 20:57:28 +0100

    New commit

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""Python script that takes your GitHub credentials
+(username and password) and
+uses the GitHub API to display your id"""
 if __name__ == "__main__":
     import requests
     import sys
