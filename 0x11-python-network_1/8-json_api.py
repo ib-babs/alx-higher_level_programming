6b91414df6dd4d2dd0889bacd847cefb14ffd6f1
ib-babs <babatundeibrahim426@gmail.com>
2024-01-26 20:14:19 +0100

    New commit

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a letter
+and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    q = None
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    # URL
+    post_url = "http://0.0.0.0:5000/search_user"
+
+    req = requests.post(post_url, params=q)
+
+    # Convert output to json
+    json_req = req.json()
+
+    # Check if json_req variable exist and non-empty
+    if json_req and json_req != {}:
+        print("[{}] {}".format(json_req.get('id'),
+              json_req.get('name')))
+    elif not json_req:
+        print("Not a valid JSON")
+    elif json_req == {}:
+        print('No result')
