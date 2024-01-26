1c5d161db283e61cd0948014b48fe4a5668f83f7
ib-babs <babatundeibrahim426@gmail.com>
2024-01-26 18:11:24 +0100

    Initial commit

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
     email = sys.argv[2]
-    req = urllib.request.Request(url=url)
+    req = urllib.request.Request(url=url, method='POST',
+                                 data={'email': email})
     with urllib.request.urlopen(req) as response:
         print(response.read())
