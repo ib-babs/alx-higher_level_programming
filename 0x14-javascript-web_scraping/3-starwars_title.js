8ba3d5fae5ef2b68438956ad0b59c68b60415657
ib-babs <babatundeibrahim426@gmail.com>
2024-02-20 16:39:26 +0100

    Initial commit

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
-// Prints the title of a Star Wars movie where the episode number
-// matches the given integer
-
 const request = require('request');
-const argv = process.argv;
-const url = 'http://swapi.co/api/films/';
-
-request(url + argv[2], function (err, res, body) {
+const endPoint = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request({ url: endPoint, methods: 'GET' }, function (err, response, body) {
   if (err) {
     console.log(err);
+  } else {
+    console.log(body && JSON.parse(body).title);
   }
-  const json = JSON.parse(body);
-  console.log(json.title);
 });
